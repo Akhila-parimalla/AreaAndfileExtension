08642296f81bb5b3a6d73947258b98271d7c772f
Akhila-parimalla <92719428+Akhila-parimalla@users.noreply.github.com>
2021-10-18 23:14:29 +0530

    updated final area program

--- computearea.py
+++ computearea.py
@@ -1,2 +1,3 @@
 radius=input("Input the radius of the Circle :")
-print("The radius of the circle with radius {} is {} ",.format(radius,area)
+area=3.141592653589793*radius*radius
+print("The radius of the circle with radius {} is {} ".format(radius,area))
